510726b7ab394d09dbb3ce6215426c1ee9eb1fa0
Louis Potok <louispotok@gmail.com>
2021-02-05 15:19:33 -0500

    finalize

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
 Instructions for use:
 * `cp config.template config.py` and fill out the values.
 
-Waiting on beeminder forum: https://forum.beeminder.com/t/error-creating-datapoints/7776
-
 Note: if you switch timezones, this may screw up a bit. That seems okay.
 
-
-Plan of attack:
-[x] GET datapoints for goal (https://api.beeminder.com/#datapoint)
-[x] find the latest
-[x] fetch games since then
-[] calc days since then
-[] POST datapoints for each
+TODO:
 [] authenticate to get 3x faster on lichess API
--- main.py
+++ main.py
@@ -14,7 +14,10 @@ from collections import defaultdict
 import config
 import pytz
 
-API_URL = f"https://www.beeminder.com/api/v1/users/{config.BEEMINDER_USERNAME}/goals/{config.BEEMINDER_GOAL_NAME}/datapoints.json"
+API_BASE = f"https://www.beeminder.com/api/v1/users/{config.BEEMINDER_USERNAME}/goals/{config.BEEMINDER_GOAL_NAME}/"
+DATAPOINTS_API = API_BASE + "datapoints.json"
+DATAPOINTS_BULK_API = API_BASE + "datapoints/create_all.json"
+
 
 DATE_STR_FORMAT = "%Y%m%d"
 
@@ -24,19 +27,25 @@ def main():
     games = get_games(latest_dp)
     daily_times = calc(games)
     datapoints = dts_to_dps(daily_times)
-    # post_datapoints(daily_times)
-    return daily_times, datapoints
-   
+    resp = post_datapoints(datapoints)
+    return resp
+
+
+def post_datapoints(datapoints):
+    payload = {"auth_token": config.BEEMINDER_AUTH_TOKEN, "datapoints": datapoints}
+    return requests.post(DATAPOINTS_BULK_API, json=payload)
+
 
 def dts_to_dps(dts):
     vals = []
     for k, v in dts.items():
         vals.append({
-            "value": v / 60,
+            "value": int((v / 60) < config.DAILY_LIMIT_MINUTES),
             "daystamp": k.strftime(DATE_STR_FORMAT),
             "requestid": k.strftime(DATE_STR_FORMAT),
-            "comment": "Added by less-chess API"
+            "comment": f"{v/60:0.2f} minutes. Added by less-chess API"
             })
+    return vals
 
 def get_games(first_day):
     """
@@ -57,7 +66,7 @@ def get_games(first_day):
 
 
 def get_datapoints():
-    url = f"{API_URL}?auth_token={config.BEEMINDER_AUTH_TOKEN}"
+    url = f"{DATAPOINTS_API}?auth_token={config.BEEMINDER_AUTH_TOKEN}"
     return requests.get(url).json()
 
 def calc(games):
