9d52437bcf2f7ad7b8b2c020b88624b4076ca519
Louis Potok <louispotok@gmail.com>
2021-06-26 09:39:17 +0700

    rewrite with new approach

--- main.py
+++ main.py
@@ -2,17 +2,13 @@
 Daily report on how much time you spent playing lichess
 
 The overall approach:
-* get the last missing beeminder datapoint
-* pull all games starting 2 days before that
-* calculate daily time spent
+* "days" are determined by config.TIMEZONE.
+* get beeminder datapoints
+* call lichess API for yesterday (midnight->midnight in TIMEZONE)
+* if the value in the comment matches (2 decimal places) -> stop
+* else -> update and do the same for previous day
+
 * use beeminder's requestid (idempotency key) as the date, we only ever want one datapoint per date and the latest will be the most accurate.
-
-TODO: There is a bug where
-* we start from the last day without games
-* we default to 0 minutes during `calc`.
-* we fill in too far back
-
-So we end up wiping lots of days.
 """
 from datetime import datetime as dt 
 from datetime import date
@@ -20,10 +16,10 @@ from datetime import timedelta as td
 import requests
 import json
 import logging
-from collections import defaultdict
 import config
 import pytz
 import typing
+from dataclasses import dataclass
 
 API_BASE = f"https://www.beeminder.com/api/v1/users/{config.BEEMINDER_USERNAME}/goals/{config.BEEMINDER_GOAL_NAME}/"
 DATAPOINTS_API = API_BASE + "datapoints.json"
@@ -32,79 +28,88 @@ DATAPOINTS_BULK_API = API_BASE + "datapoints/create_all.json"
 
 DATE_STR_FORMAT = "%Y%m%d"
 
+@dataclass
+class Day:
+    """
+    This represents a single day in "local" time as defined by config.TIMEZONE.
+    Start and end are timestamps in milliseconds.
+    """
+    local_date: date
+    start: int
+    end: int
+
+    def formatted_date(self):
+        return self.local_date.strftime(DATE_STR_FORMAT)
+
+    def previous(self):
+        return Day(
+                local_date = self.local_date-td(days=1),
+                start = self.start - (24 * 3600 * 1000),
+                end = self.start
+                )
+
+
+
+
+@dataclass
+class Game:
+    """timestamps in milliseconds"""
+    created_at: int
+    last_move_at: int
+
+    def duration_seconds(self):
+        return (self.last_move_at - self.created_at) / 1000
+
+
 def main():
     # call beeminder API
-    existing_datapoints = get_datapoints()
-
-    # figure out start_time
-    start_time = decide_start_time(existing_datapoints)
-
-    # call lichess API
-    games = get_games(start_time)
-
-    # prepare beeminder stats
-    new_datapoints = make_new_datapoints(games, start_time)
-
-    # call beeminder API
-    resp = post_datapoints(new_datapoints)
+    existing_datapoints = convert_datapoints(get_datapoints())
+    curr = make_yesterday()
+    day_games = get_games(curr)
+    while beeminder_is_wrong(curr, existing_datapoints, day_games):
+        logging.info(f"updating for {curr}")
+        update_datapoint(curr,day_games)
+        curr = curr.previous()
+        if curr.local_date < config.START_DATE:
+            return
+        day_games = get_games(curr)
 
 
-    logging.info(f"{resp.status_code}: {resp.text}")
-    return resp
 
-def make_new_datapoints(games: list, start_time: int) -> list:
-    daily_times = calc(games)
-    daily_times = fill_in(daily_times, to_day(start_time))
-    datapoints = dts_to_dps(daily_times)
-    return datapoints
+    return
 
+def convert_datapoints(datapoints: list) -> typing.Dict[str, dict]:
+    return_val = {}
+    for d in datapoints:
+        k = d['requestid']
+        if k in return_val:
+            raise ValueError(f"duplicate requestid {k} in beeminder datapoints")
+        return_val[k] = d
+    return return_val
 
-def decide_start_time(datapoints: list) -> int:
-    dates = sorted([dt.strptime(dp['daystamp'], DATE_STR_FORMAT).date() for dp in datapoints])
-    day_start = get_first_missing_date(dates)
-    start_time = get_start_time(day_start)
-    return start_time
-
-def fill_in(daily_times: dict, start_time: date) -> dict:
-    curr = start_time
-    while curr < dt.now().date():
-        daily_times[curr] += 0
-        curr += td(days=1)
-    return daily_times
-
-
-def get_first_missing_date(dates: typing.List[date]) -> date:
-    val = dates[-1]
-    prev = dates[0]
-    for d in dates[1:]:
-        if d - prev > td(days=1):
-            return prev
-        else:
-            prev = d
-    return val
-
-def post_datapoints(datapoints: typing.List[dict]) -> requests.Response:
-    payload = {"auth_token": config.BEEMINDER_AUTH_TOKEN, "datapoints": datapoints}
-    return requests.post(DATAPOINTS_BULK_API, json=payload)
-
-
-def dts_to_dps(dts: typing.Dict[date, float]) -> typing.List[dict]:
-    vals = []
-    for k, v in dts.items():
-        vals.append({
-            "value": int((v / 60) < config.DAILY_LIMIT_MINUTES),
-            "daystamp": k.strftime(DATE_STR_FORMAT),
-            "requestid": k.strftime(DATE_STR_FORMAT),
-            "comment": f"{v/60:0.2f} minutes. Added by less-chess API"
-            })
-    return vals
-
-def get_games(start_time: int) -> list:
+def make_yesterday() -> Day:
     """
-    start_time: microseconds
+    Whatever today is in TIMEZONE, return the previous day.
+    Probably fails weirdly for DST, don't care.
     """
-    logging.info(f"getting games since {start_time}")
-    params = {"since": start_time, "moves": False}
+    now = dt.now(pytz.timezone(config.TIMEZONE))
+    yesterday = now - td(days=1)
+    start = dt_to_milli(to_midnight(yesterday))
+    end = dt_to_milli(to_midnight(now))
+    return Day(local_date=yesterday.date(),start=start, end=end)
+
+
+def to_midnight(dt_to_replace: dt) -> dt:
+    return dt_to_replace.replace(hour=0,minute=0,second=0,microsecond=0)
+
+
+def get_games(day: Day) -> typing.List[Game]:
+    """
+    """
+    start_time = day.start
+    end_time = day.end
+    logging.info(f"getting games between {start_time} and {end_time}")
+    params = {"since": start_time, "until": end_time, "moves": False}
     resp = requests.get(
         f"https://lichess.org/api/games/user/{config.LICHESS_USER_NAME}",
         params=params,
@@ -112,11 +117,55 @@ def get_games(start_time: int) -> list:
     )
     if resp.status_code == 200:
         games = [json.loads(g) for g in resp.text.splitlines()]
+        games = [Game(created_at=g['createdAt'], last_move_at=g['lastMoveAt']) for g in games]
         logging.info("fetched games")
         return games
     else:
         logging.error(f"ERROR: could not get games: {resp.text}")
         return []
+
+def beeminder_is_wrong(
+        curr: Day,
+        existing_datapoints: typing.Dict[str,dict],
+        games: typing.List[Game]
+        ):
+    """
+    1. calculate duration for day_games
+    2. compare to value from existing_datapoints
+    """
+    actual_duration = sum([g.duration_seconds() for g in games])
+
+    rel_dp = existing_datapoints.get(curr.formatted_date(), None)
+    if not rel_dp:
+        logging.info(f"missing datapoint for day {curr}")
+        return True
+
+    beeminder_duration = float(rel_dp['comment'].split()[0])
+    is_wrong = not is_close(beeminder_duration, actual_duration)
+    if is_wrong:
+        logging.info(
+                f"discrepancy between {beeminder_duration} (beeminder) and {actual_duration} (lichess) for {curr}"
+                )
+    return is_wrong
+
+
+def is_close(a,b,tol=.01):
+    return abs(a-b) < tol
+
+
+
+def update_datapoint(curr: Day, day_games: typing.List[Game]) -> None:
+    duration_minutes = sum([g.duration_seconds() for g in day_games]) / 60
+    payload = {
+            "auth_token": config.BEEMINDER_AUTH_TOKEN,
+            "value": int(duration_minutes < config.DAILY_LIMIT_MINUTES),
+            "daystamp": curr.formatted_date(),
+            "requestid": curr.formatted_date(),
+            "comment": f"{duration_minutes:0.2f} minutes ({len(day_games)} games.) Added by less-chess API"
+            }
+    logging.info(f"posting {payload} for {curr}")
+    #resp = requests.post(DATAPOINTS_API, json=payload)
+    #logging.info(f"request for {curr} returned {resp.status_code}")
 
 
 def get_datapoints() -> list:
@@ -125,29 +174,12 @@ def get_datapoints() -> list:
     j = requests.get(url).json()
     return j
 
-def calc(games: list) -> typing.Dict[date,float]:
-    """
-    Pure python way to do `games.groupby(day).sum()`
-    """
-    result: typing.Dict[date,float] = defaultdict(lambda: 0)
-    for g in games:
-        day = to_day(g["createdAt"])
-        duration = (g["lastMoveAt"] - g["createdAt"]) / 1000
-        result[day] += duration
-    return result
 
 
-def to_day(ts: int) -> date:
-    return dt.fromtimestamp(ts / 1000, tz=pytz.timezone(config.TIMEZONE)).date()
-
-
-def dt_to_micro(d: dt) -> int:
+def dt_to_milli(d: dt) -> int:
     return int(dt.timestamp(d)) * 1000
 
 
-def get_start_time(first_day: date) -> int:
-    ONE_DAY_MICRO = 24 * 3600 * 1000
-    return dt_to_micro(dt.combine(first_day,dt.min.time())) - (ONE_DAY_MICRO * 2)
 
 if __name__ == '__main__':
     logging.basicConfig(filename="./less-chess.log", level=logging.INFO, format="%(asctime)s %(message)s")
